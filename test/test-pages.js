584c6c160ca88e2feeba51892aef04c3534bb43d
Mandip Tharu <mandip810@gmail.com>
2021-01-12 15:17:53 +0545

    test case fixes

--- test/test-pages.js
+++ test/test-pages.js
@@ -2,36 +2,37 @@ const { expect } = require('chai')
 const supertest = require('supertest')
 const app = supertest(require('../app'))
 
-describe('Status and Content', () => {
+describe('Main page', () => {
 
-    describe('Main page', () => {
-
-        it('status', function (done) {
-            app.get('/').expect(200, done)
-        })
-
-        it('content', function (done) {
-            app.get('/').expect('Hello World', done)
-        })
-
+    it('status', function (done) {
+        app.get('/').expect(200, done)
     })
 
-    describe('About page', () => {
+    it('content', function (done) {
+        app.get('/').expect('Hello World', done)
+    })
 
-        it('status', function (done) {
-            app.get('/about').expect(200, done)
-        })
+})
 
-        it('content', function (done) {
-            app.get('/about')
-                .expect(200)
-                .end((err, res) => {
-                    if (err) return done(err)
-                    expect(res.text).to.contain('About')
-                    return done()
-                })
-        })
+describe('About page', () => {
 
+    it('status', function (done) {
+        app.get('/about').expect(200, done)
+    })
+
+    it('content', function (done) {
+        app.get('/about')
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err)
+                expect(res.text).to.contain('About')
+                return done()
+            })
+    })
+
+    // needs to be changed later
+    after(() => {
+        process.exit(0)
     })
 
 })
